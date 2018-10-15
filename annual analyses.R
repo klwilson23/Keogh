efada7f50491a06b7d57c07419b8619514c7089f
Kyle Wilson <kyle.logan.wilson@gmail.com>
2018-10-15 14:10:13 -0700

    finding recruit lag times

--- annual analyses.R
+++ annual analyses.R
@@ -4,6 +4,13 @@ keogh <- read.csv("Data/Keogh_Database_Final_01oct18.csv",stringsAsFactors = F,h
 keogh <- keogh[!keogh$species_code%in%c("cf","ctt","ctr","ks","shweres","shlgbres","cot"),]
 keogh$life_stage[keogh$life_stage=="F"] <- "f"
 keogh$life_stage[keogh$life_stage==" "] <- ""
+unique(keogh$fresh_age)
+keogh$age <- as.numeric(keogh$fresh_age)
+
+annual_age <- aggregate(number~year+species+life_stage+age,data=keogh,FUN=sum,na.rm=T)
+colnames(annual_age) <- c("Year","Species","Stage","Age","Abundance")
+
+steel_age <- subset(annual_age,Species=="sh"&Stage=="s")
 
 annual <- aggregate(number~year+species+life_stage,data=keogh,FUN=sum,na.rm=T)
 colnames(annual) <- c("Year","Species","Stage","Abundance")
@@ -17,6 +24,10 @@ colnames(size) <- c("Year","Species","Stage","Length","Sigma")
 
 
 library(reshape2)
+annual_cohort <- dcast(annual_age[annual_age$Stage=="s",],Year~Species+Age,value.var="Abundance")
+sh_lag <- aggregate(Age~Year,data=annual_age[annual_age$Species=="sh"&annual_age$Stage=="s",],mean,na.rm=T)
+dv_lag <- aggregate(Age~Year,data=annual_age[annual_age$Species=="dv"&annual_age$Stage=="s",],mean,na.rm=T)
+ct_lag <- aggregate(Age~Year,data=annual_age[annual_age$Species=="ct"&annual_age$Stage=="s",],mean,na.rm=T)
 
 annual_abund <- dcast(annual,Year~Species+Stage,value.var="Abundance")
 annual_size <- dcast(size,Year~Species+Stage,value.var="Length")
@@ -40,3 +51,4 @@ plot(ct_s~ct_a,data=annual_abund,pch=21,bg="grey50")
 lines(smooth.spline(annual_abund$ct_a[!is.na(annual_abund$ct_s) & !is.na(annual_abund$ct_a)],annual_abund$ct_s[!is.na(annual_abund$ct_s) & !is.na(annual_abund$ct_a)],cv=F),lwd=2,lty=2,col="dodgerblue")
 
 plot(co_s~co_a,data=annual_abund,pch=21,bg="grey50")
+
