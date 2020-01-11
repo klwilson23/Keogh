065a882824165a0b728e25e4ee009597f75b6fca
Kyle Wilson <kyle.logan.wilson@gmail.com>
2020-01-10 16:33:59 -0800

    Changing up linkage between adult run and survival

--- Keogh Stan run v2.R
+++ Keogh Stan run v2.R
@@ -6,18 +6,17 @@ library(ggplot2)
 library(MARSS)
 library(broom)
 library(wesanderson)
-library(devtools)
-#devtools::install_github("nwfsc-timeseries/atsar")
-library(atsar)
 library(rstan)
+library(loo)
 library(rethinking)
 
 rstan_options(auto_write = TRUE)
-#Sys.setenv(LOCAL_CPPFLAGS = '-march=native')
+options(mc.cores = parallel::detectCores())
 
 keogh <- readRDS("Keogh_newJuv_enviro.rds")
 run_time <- readRDS("Data/steelhead_run.rds")
 sh_annual <- readRDS("Data/steelhead_run_annual.rds")
+sh_annual$time <- 1:length(sh_annual$year)
 keogh_long <- subset(keogh,Year<=2015 & Year>=1976)
 keogh_long <- subset(keogh_long,Species!="Chum")
 keogh_long$Species <- factor(keogh_long$Species,levels=unique(keogh_long$Species))
@@ -25,23 +24,25 @@ keogh_long$marSurv <- keogh_long$Stock/keogh_long$juvCohort
 keogh_long$logitSurv <- log(keogh_long$marSurv/(1-keogh_long$marSurv))
 keogh_long$prod <- log(keogh_long$Recruits/keogh_long$Stock)
 
-X <- model.matrix(~-1+Species+Stock:Species+sumRain:Species+sumTemp:Species+winRain:Species+winTemp:Species,data=keogh_long)
+X <- model.matrix(~Species+Stock:Species+sumRain:Species+sumTemp:Species+winRain:Species+winTemp:Species,data=keogh_long)
 
-trends <- model.matrix(~-1+logitSurv:Species+seals:Species,data=keogh_long)
+trends <- model.matrix(~logitSurv:Species+seals:Species,data=keogh_long)
 
-Xvars <- c("seals","npgo")
-sdSurv_sh <- attr(scale(sh_annual[,Xvars],center=TRUE,scale=TRUE),"scaled:center")
+Xvars <- c("seals","npgo","mei","oceanSalmon")
+sdSurv_sh <- attr(scale(sh_annual[,Xvars],center=TRUE,scale=TRUE),"scaled:scale")
 mnSurv_sh <- attr(scale(sh_annual[,Xvars],center=TRUE,scale=TRUE),"scaled:center")
 enviro <- scale(sh_annual[,Xvars],center=TRUE,scale=TRUE)
-enviro <- data.frame(enviro)
-sh_trends <- model.matrix(~-1+seals+npgo,data=enviro)
+enviro <- data.frame(Xvars=enviro)
+colnames(enviro) <- Xvars
+sh_trends <- model.matrix(~seals+npgo+mei+oceanSalmon,data=enviro)
 
-XXvars <- c("total_rain_run","mean_temp_run")
-sdSurv_run <- attr(scale(sh_annual[,XXvars],center=TRUE,scale=TRUE),"scaled:center")
+XXvars <- c("total_rain_run","mean_temp_run","sex")
+sdSurv_run <- attr(scale(sh_annual[,XXvars],center=TRUE,scale=TRUE),"scaled:scale")
 mnSurv_run <- attr(scale(sh_annual[,XXvars],center=TRUE,scale=TRUE),"scaled:center")
 enviro_run <- scale(sh_annual[,XXvars],center=TRUE,scale=TRUE)
 enviro_run <- data.frame(enviro_run)
-run_trends <- model.matrix(~-1+total_rain_run+mean_temp_run,data=enviro_run)
+colnames(enviro_run) <- XXvars
+run_trends <- model.matrix(~total_rain_run+mean_temp_run+sex,data=enviro_run)
 
 plot(sh_annual$total_rain_run,sh_annual$run)
 
@@ -52,26 +53,43 @@ dat <- list("N"=nrow(sh_trends),
             "J"=ncol(run_trends),
             "XX"=run_trends,
             "run_time"=sh_annual$run)
-trackPars <- c("beta_surv","pS0","beta_run","run0","sigma_surv","sigma_run","surv_new","run_new")
 
-fit <- stan(file = "Stan code/Keogh Surv.stan", pars=trackPars,data=dat, iter=5000,chains=4,cores=4,control=list("adapt_delta"=0.8))
+fit <- stan(file = "Stan code/Keogh Surv.stan", data=dat, iter=5000,chains=1,cores=1,control=list("adapt_delta"=0.8))
 
-summary(fit, pars=trackPars[!grepl("new",trackPars)],probs=c(0.025,0.975))$summary
+summary(fit, pars=c("beta_surv","beta_run","sigma_surv","sigma_run","phi_surv","phi_run"),probs=c(0.025,0.975))$summary
 mypost <- as.data.frame(fit)
-surv_ppd <- mypost[,grep("surv_new",colnames(mypost))]
+surv_ppd <- extract(fit)$surv_new
 mn_ppd <- colMeans(surv_ppd)
 ci_ppd <- apply(surv_ppd,2,HPDI,prob=0.89)
 plot(1/(1+exp(-dat$lSurv)),mn_ppd,pch=21,bg="grey50",ylim=range(ci_ppd),xlim=range(1/(1+exp(-dat$lSurv))), main = "Survival",xlab="Observed survival",ylab="Posterior predictive")
 segments(x0=1/(1+exp(-dat$lSurv)),y0=ci_ppd[1,],y1=ci_ppd[2,],lwd=1)
 abline(b=1,a=0,lwd=2,lty=1,col="red")
 
-
-run_ppd <- mypost[,grep("run_new",colnames(mypost))]
+run_ppd <- extract(fit)$run_new
 mn_ppd <- colMeans(run_ppd)
 ci_ppd <- apply(run_ppd,2,HPDI,prob=0.89)
 plot(dat$run_time,mn_ppd,pch=21,bg="grey50",ylim=range(ci_ppd),xlim=range(dat$run_time), main = "Run time",xlab="Observed run time",ylab="Posterior predictive")
 segments(x0=dat$run_time,y0=ci_ppd[1,],y1=ci_ppd[2,],lwd=1)
 abline(b=1,a=0,lwd=2,lty=1,col="red")
 
-plot(colMeans(run_ppd))
-plot(colMeans(surv_ppd))
+run_ci <- apply(extract(fit)$run_new,2,HPDI,prob=0.95)
+plot(colMeans(run_ppd),ylim=range(run_ci),lwd=2,type="l")
+polygon(x=c(1:nrow(dat$X),rev(1:nrow(dat$X))),y=c(run_ci[1,],rev(run_ci[2,])),col=adjustcolor("grey",0.5))
+lines(colMeans(run_ppd),lwd=2)
+points(dat$run_time,pch=21,bg="red")
+
+surv_ci <- apply(extract(fit)$surv_new,2,HPDI,prob=0.95)
+plot(colMeans(extract(fit)$surv_new),ylim=range(surv_ci),lwd=2,type="l")
+polygon(x=c(1:nrow(dat$X),rev(1:nrow(dat$X))),y=c(surv_ci[1,],rev(surv_ci[2,])),col=adjustcolor("grey",0.5))
+lines(colMeans(extract(fit)$surv_new),lwd=2)
+
+# seals:
+surv_ppd <- extract(fit)$surv_new[,order(sh_annual$seals)]
+surv_ci <- apply(surv_ppd,2,HPDI,prob=0.95)
+plot(sort(sh_annual$seals),colMeans(surv_ppd),ylim=range(surv_ci),lwd=2,type="l")
+polygon(x=c(sort(sh_annual$seals),rev(sort(sh_annual$seals))),y=c(surv_ci[1,],rev(surv_ci[2,])),col=adjustcolor("grey",0.5))
+lines(sort(sh_annual$seals),colMeans(surv_ppd),lwd=2)
+
+plot(sh_annual$seals,colMeans(extract(fit)$surv_new))
+plot(sh_annual$total_rain_run,colMeans(extract(fit)$run_new))
+loo(extract_log_lik(fit,parameter_name = c("log_lik1","log_lik2")),cores=1)
--- Stan code/Keogh Surv.stan
+++ Stan code/Keogh Surv.stan
@@ -10,41 +10,60 @@ data {
 
 parameters {
   vector[K] beta_surv;
-  real pS0;
   vector[J] beta_run;
-  real run0;
   real<lower=0> sigma_surv;
   real<lower=0> sigma_run;
+  real<lower=-1,upper=1> phi_surv;
+  real<lower=-1,upper=1> phi_run;
 }
 
 transformed parameters{
   vector[N] mnSurv;
   vector[N] mnRun;
-  mnSurv = pS0 + X * beta_surv;
-  mnRun = run0 + XX * beta_run;
+  vector[N] epsilonSurv;
+  vector[N] epsilonRun;
+  real sigma_cor_surv;
+  real sigma_cor_run;
+  mnSurv[1] = X[1] * beta_surv;
+  epsilonSurv[1] = lSurv[1] - mnSurv[1];
+  mnRun[1] = XX[1] * beta_run;
+  epsilonRun[1] = run_time[1] - mnRun[1];
+  for(i in 2:N) {
+    mnSurv[i] = X[i] * beta_surv + phi_surv*epsilonSurv[i-1];
+    epsilonSurv[i] = (lSurv[i] - mnSurv[i]) - phi_surv*epsilonSurv[i-1];
+    mnRun[i] = XX[i] * beta_run + phi_run*epsilonRun[i-1];
+    epsilonRun[i] = (run_time[i] - mnRun[i]) - phi_run*epsilonRun[i-1];
+  }
+  sigma_cor_surv = sqrt( sigma_surv*sigma_surv * (1-phi_surv*phi_surv));
+  sigma_cor_run = sqrt( sigma_run*sigma_run * (1-phi_run*phi_run));
 }
 
 model {
-  // declare priors on initial states
-  run0 ~ normal(mean(run_time),50);
-  pS0 ~ normal(0,5);
-  // variances for process and observation error
-  sigma_surv ~ cauchy(0,5);
-  sigma_run ~ cauchy(0,5);
+  // declare priors on autocorrelation
+  phi_surv ~ normal(0,1);
+  phi_run ~ normal(0,1);
   // regression coefficients
-  beta_surv ~ normal(0,5);
-  beta_run ~ normal(0,5);
+  beta_surv[1] ~ normal(mean(lSurv),sd(lSurv));
+  beta_run[1] ~ normal(mean(run_time),sd(run_time));
+  if(K>1) beta_surv[2:K] ~ normal(0,5);
+  if(J>1) beta_run[2:J] ~ normal(0,5);
+  // variances for process error
+  sigma_surv ~ cauchy(0,5);
+  sigma_run ~ cauchy(0,25);
   // likelihood below
-  lSurv ~ normal(mnSurv,sigma_surv);
-  run_time ~ normal(mnRun,sigma_run);
-  
+  lSurv ~ normal(mnSurv,sigma_cor_surv);
+  run_time ~ normal(mnRun,sigma_cor_run);
 }
 
 generated quantities {
+  vector[N] log_lik1;
+  vector[N] log_lik2;
   vector[N] surv_new;
   vector[N] run_new;
   for(i in 1:N) {
-    surv_new[i] = inv_logit(normal_rng(mnSurv[i],sigma_surv));
-    run_new[i] = normal_rng(mnRun[i],sigma_run);
+    surv_new[i] = inv_logit(normal_rng(mnSurv[i],sigma_cor_surv));
+    run_new[i] = normal_rng(mnRun[i],sigma_cor_run);
+    log_lik1[i] = normal_lpdf(lSurv[i] | mnSurv[i],sigma_cor_surv);
+    log_lik2[i] = normal_lpdf(run_time[i] | mnRun[i],sigma_cor_run);
   }
 }
